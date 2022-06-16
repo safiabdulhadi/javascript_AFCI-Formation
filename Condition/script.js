d2ca7b12622681feef8a827d2dea1692bb73a74b
safiabdulhadi <safiabdulhadi1994@gmail.com>
2022-06-16 16:19:06 +0200

    commit for conditions switch

--- Condition/script.js
+++ Condition/script.js
@@ -25,6 +25,37 @@ else{
 let message = x<=10 ? x+ "Est plus petit ou égale à 10" : x+ "Est plus grand que 10";
 console.log(message);
 // Atttenstion de ne pas allé trop loin avec le sternaires pour rester visible ici:
-// condition ? true : conditon ? true : false;
+// condition ? true : conditon ? true : false
 let m2 = x<10? x+ " Est plus petit que 10" : x>10? x + "Est plus grand que 10" :"Il vaut 10"
 console.log(m2);
+
+
+
+
+
+
+// ----------------------SWITCH--------------------
+let ville = prompt("De quel ville vezez-vous?");
+console.log(ville.toLowerCase());
+
+
+// .toLowerCase() transforme toute les lettres d'un string en minuscule
+switch(ville.toLowerCase()){
+    // switch compare ce qui est mit entre parenthèse et selectionne le case qui correspond
+    case "paris":
+        case "londre":
+            case "tokyo":
+        console.log("De la capitale donc.");
+        break;
+        case "lille":
+            console.log("Moule frite et bière");
+            break;
+            case "roubaix":
+                console.log("l'ancienene capitale du textile");
+                break;
+                case "armentière":
+                    console.log("Une ville pauvre mais fière");
+                    break;
+                    default:
+                        console.log("Je ne connais pas.");
+}
